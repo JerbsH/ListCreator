333caccfbb498d74a3de34ec2cabd5b32b7215e8
Jere <101744771+JerbsH@users.noreply.github.com>
2024-05-14 16:50:02 +0300

    Started scripts

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getAccessToken } from './getprofile';
+
 
 export default function App() {
+  getAccessToken(process.env.CLIENT_ID, 'code')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>UI TEST</Text>
       <StatusBar style="auto" />
     </View>
   );
